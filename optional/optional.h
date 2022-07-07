6dbf5553e769a865a19120c0e2b4b79cc5df5874
Log-in-off <1386348@mail.ru>
2022-07-07 15:49:16 +0300

    реализован optional

--- optional/optional.h
+++ optional/optional.h
@@ -17,80 +17,159 @@ public:
     Optional() = default;
     Optional(const T& value)
     {
-
+        obj_ = new (data_) T(value);
+        is_initialized_ = true;
     }
     Optional(T&& value)
     {
+        obj_ = new (data_) T(std::move(value));
+        is_initialized_ = true;
 
     }
+
     Optional(const Optional& other)
     {
-
+        is_initialized_ = false;
+        if (other.HasValue())
+        {
+            obj_ = new (data_) T(other.Value());
+            is_initialized_ = true;
+        }
     }
+
     Optional(Optional&& other)
     {
-
+        is_initialized_ = false;
+        if (other.HasValue())
+        {
+            obj_ = new (data_) T(std::move(other.Value()));
+            is_initialized_ = true;
+        }
     }
 
     Optional& operator=(const T& value)
     {
+        if (HasValue())
+        {
+            *obj_ = value;
+        }
+        else
+        {
+            obj_ = new (data_) T(value);
+        }
+        is_initialized_ = true;
+        return *this;
 
     }
+
     Optional& operator=(T&& rhs)
     {
-
+        if (HasValue())
+        {
+            *obj_ = std::move(rhs);
+        }
+        else
+        {
+            obj_ = new (data_) T(std::move(rhs));
+        }
+        is_initialized_ = true;
+        return *this;
     }
+
     Optional& operator=(const Optional& rhs)
     {
+        if (rhs.HasValue())
+        {
+            if (HasValue())
+            {
+                *obj_ = rhs.Value();
+            }
+            else
+            {
+                obj_ = new (data_) T(rhs.Value());
+            }
+            is_initialized_ = true;
+        }
+        else if (HasValue())
+            Reset();
 
+        return *this;
     }
+
     Optional& operator=(Optional&& rhs)
     {
+        if (rhs.HasValue())
+        {
+            if (HasValue())
+            {
+                *obj_ = std::move(rhs.Value());
+            }
+            else
+            {
+                obj_ = new (data_) T(std::move(rhs.Value()));
+            }
+            is_initialized_ = true;
+        }
+        else if (HasValue())
+            Reset();
+
+        return *this;
 
     }
 
     ~Optional()
     {
-
+        Reset();
     }
 
     bool HasValue() const
     {
-
+        return is_initialized_;
     }
 
     // Операторы * и -> не должны делать никаких проверок на пустоту Optional.
     // Эти проверки остаются на совести программиста
     T& operator*()
     {
-
+        return *obj_;
     }
     const T& operator*() const
     {
-
+        return *obj_;
     }
+
     T* operator->()
     {
-
+        return obj_;
     }
+
     const T* operator->() const
     {
-
+        return obj_;
     }
 
     // Метод Value() генерирует исключение BadOptionalAccess, если Optional пуст
     T& Value()
     {
-        if (!is_initialized_)
+        if (!HasValue())
             throw BadOptionalAccess{};
+        return *obj_;
     }
     const T& Value() const
     {
-        return Value();
+        if (!HasValue())
+            throw BadOptionalAccess{};
+        return *obj_;
     }
 
     void Reset()
     {
+        if (is_initialized_)
+        {
+            is_initialized_ = false;
+            obj_->~T();
+            obj_ = nullptr;
+        }
 
     }
 
@@ -98,4 +177,5 @@ private:
     // alignas нужен для правильного выравнивания блока памяти
     alignas(T) char data_[sizeof(T)];
     bool is_initialized_ = false;
+    T *obj_ = nullptr;
 };
