3490894f3181d14413adaecd56c2eaf4e70cbaf7
Log-in-off <1386348@mail.ru>
2022-07-09 22:23:03 +0300

    оптимизация

--- optional/vector.h
+++ optional/vector.h
@@ -183,22 +183,13 @@ public:
 
     void PushBack(const T& value)
     {
-
         if (size_ < data_.Capacity())
             new (data_ + size_) T((value));
         else
         {
             RawMemory<T> new_data(size_?size_*2:1);
             new (new_data + size_) T((value));
-
-            if constexpr (std::is_nothrow_move_constructible_v<T> || !std::is_copy_constructible_v<T>) {
-                std::uninitialized_move_n(data_.GetAddress(), size_, new_data.GetAddress());
-                }
-            else {
-                std::uninitialized_copy_n(data_.GetAddress(), size_, new_data.GetAddress());
-                }
-            std::destroy_n(data_.GetAddress(), size_);
-            data_.Swap(new_data);
+            SwapData(new_data);
         }
         size_++;
     }
@@ -211,15 +202,7 @@ public:
         {
             RawMemory<T> new_data(size_?size_*2:1);
             new (new_data + size_) T(std::move(value));
-
-            if constexpr (std::is_nothrow_move_constructible_v<T> || !std::is_copy_constructible_v<T>) {
-                std::uninitialized_move_n(data_.GetAddress(), size_, new_data.GetAddress());
-                }
-            else {
-                std::uninitialized_copy_n(data_.GetAddress(), size_, new_data.GetAddress());
-                }
-            std::destroy_n(data_.GetAddress(), size_);
-            data_.Swap(new_data);
+            SwapData(new_data);
         }
         size_++;
     }
@@ -236,7 +219,12 @@ public:
             return;
         }
         RawMemory<T> new_data(new_capacity);
+        SwapData(new_data);
+    }
 
+private:
+    void SwapData(RawMemory<T> &new_data)
+    {
         if constexpr (std::is_nothrow_move_constructible_v<T> || !std::is_copy_constructible_v<T>) {
             std::uninitialized_move_n(data_.GetAddress(), size_, new_data.GetAddress());
             }
@@ -246,8 +234,6 @@ public:
         std::destroy_n(data_.GetAddress(), size_);
         data_.Swap(new_data);
     }
-
-private:
     RawMemory<T> data_;
     size_t size_ = 0;
 };
